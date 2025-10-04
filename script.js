5c0dd1fd3c94f37f48fdc61043c5776569ae95d1
carineRezende <carined.rezen@gmail.com>
2025-10-03 23:46:20 -0300

    i  ctor navigation functionality to improve user experience on menu interactions.

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const btnMenu = document.getElementById('btnMenu');
-
 btnMenu.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
 
-// fecha quando clica em um link
 const links = document.querySelectorAll('.navbar a');
 links.forEach(link => {
     link.addEventListener('click', () => {
