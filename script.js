8d2f026c1034d32783bc8aed5c4bd81dd7706911
carineRezende <carined.rezen@gmail.com>
2025-10-05 19:16:25 -0300

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -10,3 +10,37 @@ links.forEach(link => {
     });
 });
 
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("show");
+  });
+});
+
+
+
+const form = document.getElementById('contact-form');
+const msgStatus = document.getElementById('msg-status');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+      msgStatus.textContent = 'Enviando...';
+      msgStatus.style.color = '#0077b6';
+
+    emailjs.init({
+        publicKey: 'DlwwRFM237-p3Psed',
+    });
+
+    emailjs.sendForm("service_ei6kngi", "template_9ikwl9d", form)
+        .then((response) => {
+            msgStatus.textContent = 'Mensagem enviada com sucesso!';
+            msgStatus.style.color = 'green';
+            form.reset();
+    })
+        .catch((error) => {
+            msgStatus.textContent = "Ocorreu um erro ao enviar a mensagem.";
+            msgStatus.style.color = 'red';
+    });
+});
